429644830e92492d689106634276e6034fc0f297
Kruk-m0 <KRUK_M0@PROTON.ME>
2025-10-29 19:00:09 +0100

    Update arduino-temperature-collection.ino

--- arduino-temperature-collection.ino
+++ arduino-temperature-collection.ino
@@ -2,7 +2,7 @@
 #define Dallas A5
 #include <OneWire.h>
 #include <DallasTemperature.h>
-OneWire oneWire(A5); //Podłączenie do A5 
+OneWire oneWire(A5); 
 DallasTemperature sensors(&oneWire);
 
 void setup() 
@@ -12,12 +12,11 @@ Serial.begin(9600); sensors.begin();
 
 void loop() 
 {
-sensors.requestTemperatures(); //Pobranie temperatury czujnika
+sensors.requestTemperatures(); 
 float temp = ((analogRead(LM35) * 5.0) / 1024.0) * 100; 
-//Serial.print("Aktualna temperatura Dallas: "); 
-Serial.print(sensors.getTempCByIndex(0)); //Wyswietlenie informacji 
+
+Serial.print(sensors.getTempCByIndex(0)); 
 Serial.print(", ");
-//Serial.print(" Aktualna temperatura LM35: "); 
 Serial.println(temp);
 delay(10000); 
 }
